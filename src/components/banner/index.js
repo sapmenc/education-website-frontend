bf1c5b480eae17799fae0f8c672a476ac6b4fcc5
dubeyom <omdubey9977@gmail.com>
2023-02-27 14:27:13 +0530

    ui change

---
+++ src/components/banner/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Banner from "./Banner";
+function Herobanner() {
+  return (
+    <div>
+      <Banner />
+    </div>
+  );
+}
+
+export default Herobanner;
---
+++ src/components/navbar/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NavLeft from "./NavLeft";
+
+function Navbar() {
+  return (
+    <nav>
+      <NavLeft />
+    </nav>
+  );
+}
+
+export default Navbar;
