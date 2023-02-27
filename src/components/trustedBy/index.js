6ade5eeebe14622e860a992b2c8694d541d28fc7
dubeyom <omdubey9977@gmail.com>
2023-02-27 21:54:22 +0530

    trusted-partial-2

--- src/components/trustedBy/index.js
+++ src/components/trustedBy/index.js
@@ -1,40 +1,53 @@
+import Image from "next/image";
+
 function TrustedBy() {
   return (
-    <div>
-      <div className="ml-10px  text-center">
-        <span className="text-[#ff7e84] text-2xl font-bold">Trusted</span>
-        <span className="ml-2 text-[#0c1327] font-bold text-2xl">
+    <div className="flex flex-col items-center py-20 gap-12 px-5">
+      <div className=" text-center ">
+        <span className="text-[#ff7e84] text-2xl underlined-red ">
+          Trusted{" "}
+        </span>
+        <span className=" text-[#0c1327]  text-2xl">
           By 10000+ young minds around the world!
         </span>
       </div>
-      <div className=" ml-10px  text-center">
-        <div className="flex justify-evenly ">
-          <img
-            className="hover:cursor-pointer ease-out duration-500 grayscale hover:grayscale-0 p-4 "
-            src="/assets/images/all-img/brands/1.svg"
-            alt="i am here"
-          />
-          <img
-            className="hover:cursor-pointer ease-out duration-500 grayscale hover:grayscale-0 p-4"
-            src="/assets/images/all-img/brands/2.svg"
-            alt="i am here"
-          />
-          <img
-            className="hover:cursor-pointer ease-out duration-500 grayscale hover:grayscale-0 p-4"
-            src="/assets/images/all-img/brands/3.svg"
-            alt="i am here"
-          />
-          <img
-            className="hover:cursor-pointer ease-out duration-500 grayscale hover:grayscale-0 p-4"
-            src="/assets/images/all-img/brands/4.svg"
-            alt="i am here"
-          />
-          <img
-            className="hover:cursor-pointer ease-out duration-500 grayscale hover:grayscale-0 p-4"
-            src="/assets/images/all-img/brands/5.svg"
-            alt="i am here"
-          />
-        </div>
+
+      <div className="flex flex-wrap justify-center gap-10  md:gap-20 max-w-[1700px] ">
+        <Image
+          className="hover:cursor-pointer ease-out duration-500 grayscale hover:grayscale-0 object-contain   "
+          src="/assets/images/all-img/brands/1.svg"
+          alt="i am here"
+          width={150}
+          height={75}
+        ></Image>
+        <Image
+          className="hover:cursor-pointer ease-out duration-500 grayscale hover:grayscale-0 "
+          src="/assets/images/all-img/brands/2.svg"
+          alt="i am here"
+          width={150}
+          height={75}
+        ></Image>
+        <Image
+          className="hover:cursor-pointer ease-out duration-500 grayscale hover:grayscale-0 "
+          src="/assets/images/all-img/brands/3.svg"
+          alt="i am here"
+          width={150}
+          height={75}
+        ></Image>
+        <Image
+          className="hover:cursor-pointer ease-out duration-500 grayscale hover:grayscale-0 "
+          src="/assets/images/all-img/brands/4.svg"
+          alt="i am here"
+          width={150}
+          height={75}
+        ></Image>
+        <Image
+          className="hover:cursor-pointer ease-out duration-500 grayscale hover:grayscale-0 "
+          src="/assets/images/all-img/brands/5.svg"
+          alt="i am here"
+          width={150}
+          height={75}
+        ></Image>
       </div>
     </div>
   );
